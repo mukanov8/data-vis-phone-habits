0ab280d1dc951982f53c868bee4adb93d62efcc2
BaurTokenov <baur.tokenov@kaist.ac.kr>
2021-11-06 15:50:47 +0900

    plotly example

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
+import React from 'react'
+
+import Plot from 'react-plotly.js'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3, 4, 5, 6, 7],
+            y: [2, 6, 3, 4, 5, 8, 5],
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: { color: 'red' },
+            title: 'sobaka',
+          },
+        ]}
+        layout={{ width: 320, height: 240, title: 'Hello World' }}
+      />
     </div>
   )
 }
