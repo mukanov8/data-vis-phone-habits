7e223a4f4cff3073c46d6db35dd5f05e4f11d98b
BaurTokenov <baur.tokenov@kaist.ac.kr>
2021-11-08 20:53:16 +0900

    apps pie chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react'
 
-import Plot from 'react-plotly.js'
+import styled from 'styled-components'
+import AppsBarChart from './components/AppsBarChart'
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: max-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 function App() {
   return (
-    <div className="App">
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3, 4, 5, 6, 7],
-            y: [2, 6, 3, 4, 5, 8, 5],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: { color: 'red' },
-            title: 'sobaka',
-          },
-        ]}
-        layout={{ width: 320, height: 240, title: 'Hello World' }}
-      />
-    </div>
+    <MainContainer>
+      <div className="ml-9">sobaka</div>
+      <AppsBarChart />
+    </MainContainer>
   )
 }
 
--- src/components/AppsBarChart.js
+++ src/components/AppsBarChart.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
+import Plot from 'react-plotly.js'
+
+const colors = ['#855CF8', '#E289F2', '#503795', '#ACB9FF', 'rgb(0, 56, 109)']
+
 const AppsBarChart = () => {
-  return <div> AppsBarChart </div>
+  return (
+    <Plot
+      layout={{ width: 350, height: 400, title: 'App Usage Statistics' }}
+      data={[
+        {
+          values: [40, 23, 17, 20],
+          labels: ['SNS', 'OS', 'Health', 'Others'],
+          marker: {
+            colors: colors,
+          },
+          hoverinfo: 'label+percent',
+
+          type: 'pie',
+        },
+      ]}
+    />
+  )
 }
 
 export default AppsBarChart
