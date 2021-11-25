809fd5d24968e918c0e9be62e546731d5410bf2f
Ayan Mukanov <mukanov.ayan@gmail.com>
2021-11-25 10:59:22 +0900

    change app text color depending on background

--- src/components/AppsSelection.js
+++ src/components/AppsSelection.js
@@ -17,6 +17,14 @@ const GridContainer = styled.div`
   margin-top: 100px;
 `
 
+function pickTextColorBasedOnBgColorSimple(bgColor, lightColor, darkColor) {
+  var color = bgColor.charAt(0) === '#' ? bgColor.substring(1, 7) : bgColor
+  var r = parseInt(color.substring(0, 2), 16) // hexToR
+  var g = parseInt(color.substring(2, 4), 16) // hexToG
+  var b = parseInt(color.substring(4, 6), 16) // hexToB
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? darkColor : lightColor
+}
+
 const AppsSelection = ({ selectedApps, removeAppFromSelection }) => {
   return (
     <MainContainer>
@@ -38,6 +46,11 @@ const AppsSelection = ({ selectedApps, removeAppFromSelection }) => {
                 display="flex"
                 flexDirection="row"
                 alignItems="center"
+                color={pickTextColorBasedOnBgColorSimple(
+                  app.color,
+                  'white',
+                  'black'
+                )}
                 p="6px 10px"
                 borderRadius="5px"
                 fontWeight="500"
